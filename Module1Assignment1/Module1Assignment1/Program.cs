7cb7b09d6bc40ac72bf1cc85e3666bf469389d78
tinycoder2123 <70987474+tinycoder2123@users.noreply.github.com>
2021-06-17 13:11:04 -0600

    Simple FizzBuzz game
    FizzBuzz is a group word game for children to teach them about division. Players take turns to count incrementally, replacing any number divisible by three with the word fizz, any number divisible by five with the word buzz, and any number divisible by both with fizzbuzz.
    
    Some interviewers give applicants simple FizzBuzz-style problems to solve during interviews. Most good programmers should be able to write out on paper or whiteboard a program to output a simulated FizzBuzz game in under a couple of minutes.
    
    Create a console application that outputs a simulated FizzBuzz game counting up to 100. The output should look something like this:
    
    1, 2, Fizz, 4, Buzz, Fizz, 7, 8, Fizz, Buzz, 11, Fizz, 13, 14, FizzBuzz,
    16, 17, Fizz, 19, Buzz, Fizz, 22, 23, Fizz, Buzz, 26, Fizz, 28, 29, FizzBuzz, 31,
    32, Fizz, 34, Buzz, Fizz, 37, 38, Fizz, Buzz, 41, Fizz, 43, 44, FizzBuzz, 46,
    47, Fizz, 49, Buzz, Fizz, 52, 53, Fizz, Buzz, 56, Fizz, 58, 59, FizzBuzz, 61,
    62, Fizz, 64, Buzz, Fizz, 67, 68, Fizz, Buzz, 71, Fizz, 73, 74, FizzBuzz, 76,
    77, Fizz, 79, Buzz, Fizz, 82, 83, Fizz, Buzz, 86, Fizz, 88, 89, FizzBuzz, 91,
    92, Fizz, 94, Buzz, Fizz, 97, 98, Fizz, Buzz.

---
+++ Module1Assignment1/Module1Assignment1/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace Module1Assignment1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = 100;
+            for (int i = 1; i <= n; i++)
+            {
+                if (i % 15 == 0)
+                    Console.Write("FizzBuzz" + " ");
+                else if (i % 3 == 0)
+                    Console.Write("Fizz" + " ");
+                else if (i % 5 == 0)
+                    Console.Write("Buzz" + " ");
+                else
+                    Console.Write(i + " ");
+            }
+        }
+    }
+}
+
